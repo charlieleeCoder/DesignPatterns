f3abc6499864e25638e92483d9e55326100e0845
Charlie Lee <charlie.lee.trainer@gmail.com>
2024-06-15 15:58:57 +0100

    Added some code for the selenium webdriver on an example page.

--- Builder/DataProcessor/Components/DataReaders/ExcelDataReader.cs
+++ Builder/DataProcessor/Components/DataReaders/ExcelDataReader.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Data.Analysis;
+using System.Text;
+using ClosedXML.Excel;
 
 namespace Reader;
 public class ExcelDataReader: IDataReader
@@ -6,6 +8,7 @@ public class ExcelDataReader: IDataReader
 	public DataFrame ReadData(string FilePath)
 	{
         // TODO: implement. Need to find relevant libraries.
+
         return new DataFrame();
     }
 }
--- Builder/DataProcessor/Components/FileSenders/WebPortalFileSender.cs
+++ Builder/DataProcessor/Components/FileSenders/WebPortalFileSender.cs
@@ -1,15 +1,34 @@
-﻿namespace Sender;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V123.Network;
+
+namespace Sender;
 public class WebPortalFileSender: IFileSender
 {
     public void SendFile(string filePath, string endLocation)
     {
-        // Create HTTP connection
 
         // Run selenium webdriver
+        IWebDriver driver = new ChromeDriver();
+
+        // Create HTTP connection
+        void CreateHTTPConnection(IWebDriver driver, string webPage)
+        {
+            // To implement
+            driver.Navigate().GoToUrl(webPage);
+        }
 
         // Select file
         if (File.Exists(filePath))
         {
+            CreateHTTPConnection(driver, endLocation);
+
+            // This is just an example, usually, many steps of navigation are required.
+            IWebElement submitButton = driver.FindElement(By.Id("submit-file"));
+
+            // Assuming they don't use a plug-in, ActiveX Controller, etc.,
+            // it really is this simple to send to a submit type form element
+            submitButton.SendKeys(filePath);
 
         }
         else
