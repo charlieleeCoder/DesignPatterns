fcd311a8f441a08cd159c6650ca8fed28a5f7858
Charlie Lee <charlie.lee.trainer@gmail.com>
2024-06-28 22:11:30 +0100

    Implementing file locations component.

---
+++ Builder/DataProcessor/Components/FileLocations/IFileLocations.cs
@@ -0,0 +1,12 @@
+﻿using DataProcessor.Enums;
+
+namespace DataProcessor.Components.FileLocations;
+
+public interface IFileLocations
+{
+    Company Company             { get; set; }
+    string CompanyName          { get; set; }
+    string StartingFileLocation { get; set; }
+    string DestinationLocation  { get; set; }
+    string ArchiveFileLocation  { get; set; }
+}
---
+++ Builder/DataProcessor/Components/FileLocations/MuffinsMuffins.cs
@@ -0,0 +1,25 @@
+﻿using DataProcessor.Enums;
+
+namespace DataProcessor.Components.FileLocations;
+
+public class MuffinsMuffins : IFileLocations
+{
+
+    public Company Company { get; set; }
+    public string CompanyName { get; set; }
+    public string StartingFileLocation { get; set; }
+    public string DestinationLocation { get; set; }
+    public string ArchiveFileLocation { get; set; }
+
+    public MuffinsMuffins()
+    {
+
+        Company = DataProcessor.Enums.Company.MuffinsMuffins;
+        CompanyName = CompanyDictionary.CompanyLookup[Company];
+        StartingFileLocation = $".\\{CompanyName}";
+        DestinationLocation = $"madeupemail@{CompanyName}.com";
+        ArchiveFileLocation = $".\\Archive\\{CompanyName}";
+
+    }
+
+}
