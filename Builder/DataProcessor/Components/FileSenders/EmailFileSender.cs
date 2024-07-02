e01586c24e8c0261e5bdde780d9acf3f2f2abe10
Charlie Lee <charles.lee@pureretirement.co.uk>
2024-07-02 14:29:52 +0100

    Added email functionatilty, but with made up details.

--- Builder/DataProcessor/Components/FileSenders/EmailFileSender.cs
+++ Builder/DataProcessor/Components/FileSenders/EmailFileSender.cs
@@ -1,16 +1,48 @@
 ﻿using DataProcessor.FileLocations;
+using System.Net.Mail;
+using System.Net;
 
 namespace DataProcessor.Components.FileSenders;
 public class EmailFileSender: IFileSender
 {
     public void SendFile(IFileLocations fileLocations)
     {
-        // Select file
+        // Return early if processed file is missing
         if (!File.Exists(fileLocations.ProcessingPathFileExtension))
         {
             throw new ArgumentException($"File does not exist at {fileLocations.ProcessingPathFileExtension}");
         }
-        
 
+        // As the details below are made up
+        bool testing = true;
+
+        if (testing) 
+        {
+            return;
+        }
+
+        // Constuct email message
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress("from@example.com"),
+            Subject = fileLocations.StartingFileName,
+            Body = $"Please find {fileLocations.StartingFileName} attached."
+        };
+
+        // Select file and destination
+        Attachment attachment = new(fileLocations.ProcessingPathFileExtension);
+        mailMessage.To.Add(fileLocations.DestinationLocation);
+        mailMessage.Attachments.Add(attachment);
+
+        // Need SMTP server details, but this has
+        SmtpClient smtpClient = new("smtp.gmail.com")
+        {
+            Port = 587,
+            Credentials = new NetworkCredential("username", "password"),
+            EnableSsl = true
+        };
+
+        // Send
+        smtpClient.Send(mailMessage);
     }
 }
